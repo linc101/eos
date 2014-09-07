7aa6523bf547d35ea74f480e8dd792260104ee67
uttp <zyhuttp@gmail.com>
2014-09-07 10:22:04 +0800

    commit something

---
+++ app/controllers/UserCenter.java
@@ -0,0 +1,10 @@
+package controllers;
+
+/**
+ * Created by yehuizhang on 14-9-6.
+ */
+public class UserCenter extends CommonRender {
+    public static void userCenter(){
+        render("/usercenter/userartical.html");
+    }
+}
