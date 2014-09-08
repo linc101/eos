497d844cc89b71e6208e372714d32933b20e3922
uttp <zyhuttp@gmail.com>
2014-09-08 23:57:06 +0800

    commit something

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -2,8 +2,25 @@
  * Created by uttp on 14-9-8.
  */
 
-EOS = EOS || {};
+var EOS = EOS || {};
 
 ((function($, window){
     EOS.util = EOS.util || {};
+
+    var util = EOS.util;
+    util.UIAssert = function(text){
+        var html = "<div style='text-align:center;line-height: 120px;'>" + text +"</div>";
+        $(html).dialog({
+            title:'提示',
+            modal:true,
+            closeText: "hide",
+            width: 300,
+            height:250,
+            buttons:[
+                {text:'确定',click:function(){
+                    $(this).dialog('close');
+                }}
+            ]
+        })
+    }
 })(jQuery, window));
