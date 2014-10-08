8588c98760636f315682ae064877ac4313ef8868
uttp@homepc <zyhuttp@gmail.com>
2014-10-08 22:05:45 +0800

    commit something

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,6 +4,21 @@
 var EOS = EOS || {};
 
 ((function($, window){
-    
+    EOS.home = EOS.home || {};
+
+    EOS.home.init = EOS.home.init || {};
+
+    $.extend(EOS.home.init, {
+        doInit:function(container){
+            EOS.home.init.container = container;
+        },
+
+        tabChange:function(){
+            var container = EOS.home.init.container;
+            container.find(".article-head").unbind().click(function(){
+
+            });
+        }
+    })
 
 })(jQuery,window));
