d6edc3594398cae5050263a938e30d3b0aed0ba5
uttp <zyhuttp@gmail.com>
2014-10-20 14:48:08 +0800

    commmit something

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -41,21 +41,21 @@ var EOS = EOS || {};
             })
         },
         weiboLogin:function(){
-            console.log("weiboLogin");
-            WB2.anyWhere(function (W) {
-                W.widget.connectButton({
-                    id: "wb_connect_btn",
-                    type: '3,2',
-                    callback: {
-                        login: function (o) { //登录后的回调函数
-                            alert("login: " + o.screen_name)
-                        },
-                        logout: function () { //退出后的回调函数
-                            alert('logout');
-                        }
-                    }
-                });
-            });
+//            console.log("weiboLogin");
+//            WB2.anyWhere(function (W) {
+//                W.widget.connectButton({
+//                    id: "wb_connect_btn",
+//                    type: '3,2',
+//                    callback: {
+//                        login: function (o) { //登录后的回调函数
+//                            alert("login: " + o.screen_name)
+//                        },
+//                        logout: function () { //退出后的回调函数
+//                            alert('logout');
+//                        }
+//                    }
+//                });
+//            });
         }
     })
 
