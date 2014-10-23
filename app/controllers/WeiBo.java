c2ab9790c897bbb1062758b8e44d67e809eaba55
uttp <zyhuttp@gmail.com>
2014-10-23 21:40:21 +0800

    commit something

---
+++ app/controllers/WeiBo.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by yehuizhang on 14/10/23.
+ */
+public class WeiBo extends CommonRender{
+    private static Logger log = LoggerFactory.getLogger(WeiBo.class);
+
+    public static void index(){
+        render("/weibo/index.html");
+    }
+}
