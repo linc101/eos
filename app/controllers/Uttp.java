a691be00e4e86cd64d9851ca9e5a1d64127f3009
uttp@workspace <uttp@workspace.com>
2014-11-06 13:51:56 +0800

    commit something

---
+++ app/controllers/Uttp.java
@@ -0,0 +1,9 @@
+package controllers;
+/**
+ * Created by uttp on 14-11-6.
+ */
+public class Uttp extends CommonRender{
+    public static void index(){
+        render("/uttp/index.html");
+    }
+}
